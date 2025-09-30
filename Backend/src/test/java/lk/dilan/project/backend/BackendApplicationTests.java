826123f17346d27f2c183087e26dbafb8bf4b48f
DiilaNa <liyanaarachchidilan@gmail.com>
2025-09-30 10:06:50 +0530

    First Commit

---
+++ Backend/src/test/java/lk/dilan/project/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.dilan.project.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
